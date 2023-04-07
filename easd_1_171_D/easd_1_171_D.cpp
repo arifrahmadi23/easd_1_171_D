500298fb05d22090f19360714ff61d1fb0ffa57a
arifrahmadi23 <muhammad231arif@gmail.com>
2023-04-07 09:09:11 +0700

    menjawab soal no. 1

--- easd_1_171_D/easd_1_171_D.cpp
+++ easd_1_171_D/easd_1_171_D.cpp
@@ -1,1 +1,4 @@
+// 1. Mengapa algoritma dibutuhkan dalam menyelesaikan masalah, karena algoritma adalah rangkaian atau langka-langka yang sistematis untuk
+//    meneyelesaikan sebuah masalah jadi dalam penerapan algoritma sendiri akan mempermudah pengguna dalam menyelesaikan sebuah masalah
+//    karena suatu masalah dapat dibagi menjadi beberapa sub masalah dalam penyelesaiannya, jadi dapat mempermudah dan mempercepat penyelesaian.
 
